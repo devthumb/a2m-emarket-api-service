a742e38ac3c2f415f6ce47dd6b0176862bd0c977
dev.thumb <dev.thumb@gmail.com>
2024-11-20 16:59:33 +0900

    init

---
+++ src/main/java/kr/emarket/api/EmarketApiServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.emarket.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmarketApiServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EmarketApiServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/kr/emarket/api/EmarketApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.emarket.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmarketApiServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
