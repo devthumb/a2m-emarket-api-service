0ff85655470e2c73c3f296bc157040ea46c18b36
dev.thumb <dev.thumb@gmail.com>
2024-11-26 23:32:38 +0900

    # hmac   * policy interface 수정 적용

--- src/main/java/kr/emarket/api/config/CustomHmacRequestPolicy.java
+++ src/main/java/kr/emarket/api/config/CustomHmacRequestPolicy.java
@@ -1,8 +1,8 @@
 package kr.emarket.api.config;
 
+import com.a2m.atom.cloud.auth.hmac.dto.request.HmacRequest;
 import com.a2m.atom.cloud.auth.hmac.service.validator.HmacRequestPolicyValidator.HmacRequestPolicy;
 import com.a2m.atom.cloud.auth.hmac.service.validator.impl.DefaultHmacRequestPolicyValidator.PolicyArguments;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -11,10 +11,9 @@ import org.springframework.stereotype.Component;
 public class CustomHmacRequestPolicy implements HmacRequestPolicy {
 
   @Override
-  public boolean validate(HttpServletRequest request, PolicyArguments arguments) {
-    return arguments.isMarkedAsVerifiedHmacRequest(request)
-        || arguments.isMarkedAsHmacRequest(request)
-        || arguments.checkAllowedAndDisallowedPatterns(request.getServletPath());
-//    return true;
+  public boolean validate(HmacRequest hmacRequest, PolicyArguments arguments) {
+    return arguments.isMarkedAsVerifiedHmacRequest(hmacRequest)
+        || arguments.isMarkedAsHmacRequest(hmacRequest)
+        || arguments.checkAllowedAndDisallowedPatterns(hmacRequest.getServletPath());
   }
 }
